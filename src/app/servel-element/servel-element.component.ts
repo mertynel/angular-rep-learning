641149834347527cd25932f28fb3333d4a3c0658
mertynel <mertynel65@gmail.com>
2018-04-24 18:20:39 +0300

    final

--- src/app/servel-element/servel-element.component.ts
+++ src/app/servel-element/servel-element.component.ts
@@ -1,17 +1,62 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  Input, 
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewChecked,
+  OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'app-servel-element',
   templateUrl: './servel-element.component.html',
   styleUrls: ['./servel-element.component.css']
 })
-export class ServelElementComponent implements OnInit {
+export class ServelElementComponent implements 
+  OnInit, 
+  OnChanges, 
+  DoCheck, 
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewChecked,
+  OnDestroy {
   @Input('srvElement') element: {type: string, name: string, content: string};
 
-  constructor() { }
+  constructor() { 
+    console.log('constructor called')
+  }
 
   ngOnInit() {
+    console.log('ngOnInit called')
+  }
 
+  ngOnChanges(changes: SimpleChanges){
+    console.log('ngOnChanges called')
+    console.log(changes)
+  }
+
+  ngDoCheck(){
+    console.log('doCkeck called.')
+  }
+
+  ngAfterContentInit(){
+    console.log('afterContentInit called.')
+  }
+
+  ngAfterContentChecked(){
+    console.log('afterContentChecked called.')
+  }
+
+  ngAfterViewChecked(){
+    console.log('afterViewChecked called.')    
+  }
+
+  ngOnDestroy(){
+    console.log('onDestroy called.')    
+    
   }
 
 }
